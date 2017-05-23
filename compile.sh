1f668a07ebd6b6f5e09694b21586c50493a4ee52
Nik <bud.al.nik.1997@gmail.com>
2017-05-24 01:22:46 +0300

    some fixes

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
-g++ -o test test.cpp
-g++ -o test_cgi_parser test_cgi_parser.cpp parser.cpp
-g++ -o test_cgi_executor test_cgi_executor.cpp executor.cpp
+#!/usr/bin/env bash
+g++ -std=c++11 -o test test.cpp
+g++ -std=c++11 -o test_cgi_parser test_cgi_parser.cpp parser.cpp
+g++ -std=c++11 -o test_cgi_executor test_cgi_executor.cpp executor.cpp
